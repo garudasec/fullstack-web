f70de7fa7d69468c2490b27da2b7ec32c7901c77
garudasec <228494846+garudasec@users.noreply.github.com>
2026-06-19 20:10:16 +0530

    week2 day5 file update

---
+++ week2/day5 - js/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./component/Card";
+
+function App() {
+  return (
+    <div>
+      <h1>My First React App</h1>
+
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ week2/day5 - js/src/component/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+    return  <div>
+        <img src="https://picsum.photos/200" alt="" />
+        <h1>Name: Adarsh</h1>
+        <button>Know More</button>
+    </div>
+}
+
+export default Card;
