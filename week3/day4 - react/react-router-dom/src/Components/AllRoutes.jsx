9bf851b06cf6933c962d1f3e899344a68f3837ba
garudasec <228494846+garudasec@users.noreply.github.com>
2026-06-24 15:30:11 +0530

    react-router-dom update

--- week3/day4 - react/react-router-dom/src/Components/AllRoutes.jsx
+++ week3/day4 - react/react-router-dom/src/Components/AllRoutes.jsx
@@ -5,6 +5,8 @@ import Login from "../Pages/Login";
 import Logout from "../Pages/Logout";
 import User from "../Pages/User";
 import NotFound from "../Pages/NotFound";
+import SingleUser from "../Pages/SingleUser";
+import PrivateRoute from "../Pages/PrivateRoute";
 
 export default function AllRoutes() {
   // function Home(){
@@ -17,7 +19,15 @@ export default function AllRoutes() {
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path="/user" element={<User />} />
+        <Route
+          path="/user"
+          element={
+            <PrivateRoute>
+              <User />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/user/:userId" element={<SingleUser />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- week3/day4 - react/react-router-dom/src/main.jsx
+++ week3/day4 - react/react-router-dom/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import AuthContextProvider from "./Context/AuthContext.jsx";
 
 // step 1 import BrowserRoute component
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </BrowserRouter>
 );
