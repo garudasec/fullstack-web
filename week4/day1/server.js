d5d5b123a2d7fd8e1dac4f9357652c2d1441f52b
garudasec <228494846+garudasec@users.noreply.github.com>
2026-06-30 11:20:14 +0530

    gitignore file update

--- week4/day1/server.js
+++ week4/day1/server.js
@@ -1,8 +1,12 @@
 // const http = require('http');
 
+// http ko import kra hai node se!!
+//yaha http module hai jo server ko help karne me help kar rha hai
 import http from 'http';
 
+// yaha khudse port create kara hai
 const port = 3000
+
 const server=  http.createServer((req,res)=>{
     if(req.url == '/') {
         res.end('<h1>Welcome to the backend server</h1>')
@@ -13,4 +17,3 @@ server.listen(port, ()=> {
     console.log("server is listen in port: ", port)
 
 })
-
