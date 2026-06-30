995f837a8eeb69ee734e28ee220937a29a61ea1b
garudasec <228494846+garudasec@users.noreply.github.com>
2026-06-30 11:49:06 +0530

    week4 day2 file update

--- week4/day1/server.js
+++ week4/day1/server.js
@@ -2,18 +2,31 @@
 
 // http ko import kra hai node se!!
 //yaha http module hai jo server ko help karne me help kar rha hai
+
+
 import http from 'http';
 
-// yaha khudse port create kara hai
-const port = 3000
+const port = 3000;
 
-const server=  http.createServer((req,res)=>{
+const server = http.createServer((req,res)=>{
     if(req.url == '/') {
         res.end('<h1>Welcome to the backend server</h1>')
+    }
+    else if (req.url === '/about') {
+        res.end('<h1>This is about page!</h1>')
+    }
+    else if (req.url === '/contact') {
+        res.end('<h1>This is contact page!</h1>')
+    }
+    else if (req.url === '/home') {
+        res.end('<h1>This is HOme page!</h1>')
+    }
+
+    else {
+        res.end('<h1>404 Not Found</h1>')
     }
 })
 
 server.listen(port, ()=> {
-    console.log("server is listen in port: ", port)
-
+    console.log("Server is listen in port: ", port)
 })
