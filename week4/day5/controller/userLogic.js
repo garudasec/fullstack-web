773093eb707916313a4bf5eb0ae5813f1af20667
garudasec <228494846+garudasec@users.noreply.github.com>
2026-07-06 09:50:56 +0530

    feat: implement user management functionality with create, read, update, and delete operations

--- week4/day5/controller/userLogic.js
+++ week4/day5/controller/userLogic.js
@@ -1,11 +1,13 @@
 import User from "../model/user.js";
 
+
+// CREATE USER LOGIC
 const createUser = async (req, res) => {
   try {
     const { name, email, empId } = req.body;
     // validation
     if (!name || !email || !empId) {
-      return res.status(404).json({
+      return res.status(400).json({
         message: "data not found"
       });
     }
@@ -15,10 +17,10 @@ const createUser = async (req, res) => {
     console.log(newUser);
 
     // send response to user
-    res.status(200).json({
+    res.status(201).json({
       success: true,
-      message: "data created successfully",
-      newUser,
+      message: "Employee created successfully",
+      data: newUser
     });
   } catch (error) {
     res.status(500).json({
@@ -28,7 +30,118 @@ const createUser = async (req, res) => {
   }
 };
 
-// export default createUser
 
-export { createUser }; // multiple cheej ko export karna hai toh ye use karte hai!
+
+// GET USER LOGIC
+const getUser = async (req,res) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({
+      success:true,
+      data: users,
+      message:"Employeess fetched successfully"
+    })
+    
+  } catch (error) {
+    return res.status(500).json({
+      success:false,
+      message: error.message
+    })
+  }
+}
+
+
+// PUT (UPDATE) LOGIC
+
+const updateUser = async (req,res) => {
+  try {
+    const { empId, newName} = req.body;
+
+    if (!empId || !newName) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required"
+      })
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      {empId},
+      {name: newName},
+      {new: true}
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        success:false,
+        message: "Employee not found"
+      })
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: updatedUser,
+      message: "Employeess updated successfully"
+    })
+    
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    })
+    
+  }
+}
+
+
+// DELETE USER LOGIC
+const deleteUser = async (req, res) => {
+  try {
+    const { empId } = req.body;
+
+    // validation
+    if (!empId) {
+      return res.status(400).json({
+        success:false,
+        message: "Employee ID is required"
+      })
+    }
+
+    const deletedUser = await User.findOneAndDelete({ empId });
+
+    if (!deletedUser) {
+      return res.status(404).json({
+        success: false,
+        message: "Employee not found"
+      })
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: deletedUser,
+      message: "Employee deleted successfully"
+    })
+    
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    })
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// export default createUser, getUser, updateUser, deleteUser
+
+export { createUser, getUser, updateUser, deleteUser }; // multiple cheej ko export karna hai toh ye use karte hai!
 // isko hum object me export kar rahe hai!
--- week4/day5/model/user.js
+++ week4/day5/model/user.js
@@ -1,6 +1,20 @@
 import mongoose from "mongoose";
 
-const user = new mongoose.Schema({
+
+// here, userSchema is a schema which is a blueprint of the data that we want to store in the database
+
+// isme user ke rules honge basically,
+/*
+isme hum define karte hai like
+fields
+data types
+required or not 
+default values
+validation
+unique
+min/max length
+*/ 
+const userSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true
@@ -15,8 +29,28 @@ const user = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model('User', user)
+// here we are creating a model of the user schema, which will be used to interact with the database
+// ( User is a model name and userSchema is the schema name)
+
+/*
+is model ki help se hum database se interact karte hai for example
+
+await User.create()
+await User.find()
+await User.findOne()
+await User.findById()
+await User.findOneAndUpdate()
+await User.findOneAndDelete()
 
 
+note: 
+Hum kabhi,
+userSchema.create() nahi likhte.
+
+Hum likhte hain User.create()
+Kyuki database operations Model karta hai.
+
+*/
+const User = mongoose.model('User', userSchema)
 
 export default User
